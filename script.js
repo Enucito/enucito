a38a1f4c257ec8d685449e3beff770e767b5867b
Enucito <sebastiantobarmes@gmail.com>
2025-10-02 18:35:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Actualizar la hora en vivo
-function updateTime() {
-  const now = new Date();
-  document.getElementById('time').textContent = now.toLocaleString();
-}
-setInterval(updateTime, 1000);
-updateTime();
